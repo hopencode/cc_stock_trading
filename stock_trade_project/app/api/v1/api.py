cbef1be42c2032b8d501cf9cba0eb6b48d6f6ffc
hopencode <newalfo1006@gmail.com>
2025-06-03 17:34:08 +0900

    Delete stock_trade_project/app directory

--- stock_trade_project/app/api/v1/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import auth, admin, customer, company
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
-api_router.include_router(customer.router, prefix="/customer", tags=["customer"])
-api_router.include_router(company.router, prefix="/company", tags=["company"])
