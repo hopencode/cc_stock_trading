0b97b9825ba792da3c2488e23cdfa4271e1296d1
hopencode <newalfo1006@gmail.com>
2025-06-07 00:43:34 +0900

    Update CC_MTS_DDL.sql

--- CC_MTS_DDL.sql
+++ CC_MTS_DDL.sql
@@ -1,149 +1,51 @@
-drop view if exists customer_capital_gain_view;
-drop table if exists company_balance;
-drop table if exists customer_balance;
-drop table if exists sell_list;
-drop table if exists buy_list;
-drop table if exists order_list;
-drop table if exists financial_info;
-drop table if exists company;
-drop table if exists admin;
-drop table if exists account;
-
-create table account (
-	id varchar(10) unique,
-	password varchar(12),
-	a_number numeric(8, 0) check (a_number > 0) unique,
-	type varchar(8),
-	name varchar(20),
-	phone char(13),
-	cash bigserial check (cash >= 0),
-	capital_gain bigserial,
-	
-	primary key (a_number)
-);
-
-create table admin (
-	id varchar(10),
-	password varchar(12),
-	type char(5),
-
-	primary key (id)
-);
-
-create table company (
-	name varchar(20),
-	price bigserial check (price >= 1),
-	stock_num bigserial check (stock_num >= 1),
-	total_price bigserial check (total_price >= 1),
-	sector varchar(25),
-
-	primary key (name)
-);
+delete from company_balance;
+delete from customer_balance;
+delete from sell_list;
+delete from buy_list;
+delete from order_list;
+delete from financial_info;
+delete from company;
+delete from admin;
+delete from account;
 
 
-create table order_list (
-	a_number numeric(8, 0),
-	type varchar(4),
-	name varchar(20),
-	price bigserial check (price >= 1),
-	count bigserial check (count >= 1),
-	order_number bigserial check (order_number >= 1) unique,	
+insert into account values ('alice1', 'alice123', '12341234', 'customer', 'alice', '010-1234-1234', 10000000, 3000000);
+insert into account values ('mark1', 'mark123', '12341235', 'customer', 'mark', '010-1234-1235', 10000, 0);
+insert into account values ('chars1', 'chars123', '12341236', 'customer', 'chars', '010-1234-1236', 10000, 0);
 
-	primary key (a_number, order_number),
-	foreign key (a_number) references account
-		on delete cascade
-);
-
-create table buy_list (
-	buy_list_number bigserial check (buy_list_number >= 1) unique,
-	a_number numeric(8, 0),
-	name varchar(20),
-	price bigserial check (price >= 1),
-	b_date date,
-	b_time time,
-	b_count bigserial check (b_count >= 1),
-	s_count bigserial check (b_count >= s_count and s_count >= 0),
-	
-	primary key (a_number, buy_list_number),
-	foreign key (a_number) references account
-		on delete cascade
-);
-
-create table sell_list (
-	sell_list_number bigserial check (sell_list_number >= 1) unique,
-	a_number numeric(8, 0),
-	name varchar(20),
-	b_price bigserial check (b_price >= 1),
-	s_price bigserial check (s_price >= 1),
-	s_date date,
-	s_time time,
-	s_count bigserial check (s_count >= 1),
-
-	primary key (a_number, sell_list_number),
-	foreign key (a_number) references account
-		on delete cascade
-);
-
-create table customer_balance (
-	a_number numeric(8, 0),
-	stock_name varchar(20),
-	stock_count bigserial check (stock_count >= 0),
-	avg_buy_price float check (avg_buy_price >= 1),
-	
-
-	primary key (a_number, stock_name),
-	foreign key (a_number) references account
-		on delete cascade,
-	foreign key (stock_name) references company(name)
-		on delete cascade
-);
-
-create table company_balance (
-	a_number numeric(8, 0),
-	stock_name varchar(20),
-	stock_count bigserial check (stock_count >= 0),
-	avg_buy_price float check (avg_buy_price >= 1),
-
-	primary key (a_number, stock_name),
-	foreign key (a_number) references account
-		on delete cascade,
-	foreign key (stock_name) references company(name)
-		on delete cascade
-);
+insert into account values ('Microsoft1', 'Microsoft123', '12341237', 'company', 'Microsoft', '010-1234-1237', 10000, 0);
+insert into account values ('Ford1', 'Ford123', '12341238', 'company', 'Ford', '010-1234-1238', 10000, 0);
+insert into account values ('PG1', 'PG123', '12341239', 'company', 'Pg', '010-1234-1239', 10000, 0);
 
 
-CREATE ROLE mts_customer;
-CREATE ROLE mts_admin;
-CREATE ROLE mts_company;
-
-GRANT mts_customer TO mts_user;
-GRANT mts_admin TO mts_user;
-GRANT mts_company TO mts_user;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO mts_customer, mts_admin, mts_company;
-REVOKE INSERT, DELETE ON TABLE company FROM mts_customer, mts_company;
-GRANT ALL ON SCHEMA public TO mts_admin;
+insert into admin values ('admin1', 'admin123', 'admin');
 
 
+insert into company values ('Microsoft', 500000, 7000, 3500000000, 'IT');
+insert into company values ('Amazon', 400000, 6000, 2400000000, 'IT');
+insert into company values ('Ford', 100000, 2000, 200000000, 'consumer discretionary');
+insert into company values ('Pg', 300000, 1000, 300000000, 'consumer staple');
 
 
+insert into financial_info values ('Microsoft', 2023, 240000000, 120000000, 118000000);
+insert into financial_info values ('Microsoft', 2022, 220000000, 108000000, 96000000);
+insert into financial_info values ('Ford', 2023, 250000000, 25000000, 24000000);
+insert into financial_info values ('Pg', 2023, 120000000, 12000000, 11000000);
+
+insert into order_list values ('12341237', 'sell', 'Microsoft', 600000, 10, 1);
+insert into order_list values ('12341239', 'buy', 'Pg', 280000, 5, 2);
+
+insert into buy_list values (1, '12341237', 'Microsoft', 500000, '2024-12-07', '11:35:00', 7000, 0);
+insert into buy_list values (2, '12341238', 'Ford', 100000, '2024-12-07', '11:35:00', 2000, 0);
+insert into buy_list values (3, '12341239', 'Pg', 300000, '2024-12-07', '11:35:00', 1000, 5);
+insert into buy_list values (4, '12341234', 'Pg', 300000, '2024-12-07', '11:35:00', 5, 0);
 
 
+insert into sell_list values (1, '12341239', 'Pg', 300000, 300000, '2024-12-07', '11:35:00', 5);
 
 
+insert into customer_balance values ('12341234', 'Pg', 5, 300000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+insert into company_balance values ('12341237', 'Microsoft', 7000, 500000);
+insert into company_balance values ('12341238', 'Ford', 2000, 100000);
+insert into company_balance values ('12341239', 'Pg', 995, 300000);
